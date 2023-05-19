65abe4d5b5c0e4b0c507720d385dd0abcca82cee
JacekHacking <Jacekhacking@mac.com>
2023-05-18 18:12:26 -0600

    navbar and sidebar working as intended

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,13 @@ import Nav from './components/navigation/Nav';
 
 export default function Home() {
   return (
-    <section className="home-wrapper">
+    <>
       <Nav />
-      <h2 className="about-text">Hi, I'm Jacek!</h2>
-      <p>A Software Developer from Salt Lake City, Utah!</p>
-    </section>
+
+      <section className="home-wrapper">
+        <h2 className="about-text">Hi, I'm Jacek!</h2>
+        <p>A Software Developer from Salt Lake City, Utah!</p>
+      </section>
+    </>
   );
 }
